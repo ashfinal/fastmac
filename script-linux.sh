e9ffe19ead0449f77ff8ed11ad9fd63c747d1e03
ashfinal <ashfinal@gmail.com>
2020-10-02 02:00:19 +0800

    add custom scripts

--- script-linux.sh
+++ script-linux.sh
@@ -1,1 +1,4 @@
 echo Edit script-linux.sh in your fastmac repo to auto-run commands in your linux instances
+wget -qcO ~/.tmux.conf https://macplay.github.io/listings/manjaro/tmux.conf
+echo 'source ~/.tmux.conf' >> ~/.tmate.conf
+echo 'set tmate-authorized-keys "~/.ssh/id_qazsa"' >> ~/.tmate.conf
--- script-mac.sh
+++ script-mac.sh
@@ -1,3 +1,6 @@
 # disable spotlight indexing
 sudo mdutil -i off -a
 echo Edit script-mac.sh in your fastmac repo to auto-run commands in your Mac instances
+wget -qcO ~/.tmux.conf https://macplay.github.io/listings/manjaro/tmux.conf
+echo 'source ~/.tmux.conf' >> ~/.tmate.conf
+echo 'set tmate-authorized-keys "~/.ssh/id_ecdsa"' >> ~/.tmate.conf
